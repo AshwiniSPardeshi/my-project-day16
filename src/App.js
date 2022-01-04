2180879fd5a3721053299ad0d0de04b281795fca
Ashwini <pardeshia1998@gmail.com>
2022-01-04 15:18:24 +0530

    b4

--- src/App.js
+++ src/App.js
@@ -1,51 +1,6 @@
-import { useState } from "react";
-
-export default function App() {
-  return (
-    <>
-      <MyComponent />
-    </>
-  );
-}
-
-function MyComponent() {
-  const id1 = 100;
-  const list = [1, 1, 1, 1, 1, 1, 1, 1, 1];
-  const [cardCss, setCardcss] = useState("fs-3 p-4 mb-1");
-
-  const primaryTheme = () => setCardcss("bg-primary text-light fs-3 p-4 mb-1");
-  const darkTheme = () => setCardcss("bg-dark text-light fs-3 p-4 mb-1");
-  const dangerTheme = () => setCardcss("bg-danger text-light fs-3 p-4 mb-1");
-
-  return (
-    <div>
-      <input
-        type="button"
-        value="Primary Theme"
-        className="btn btn-primary btn-sm"
-        onClick={primaryTheme}
-      />
-      <input
-        type="button"
-        value="Primary Theme"
-        className="btn btn-dark btn-sm"
-        onClick={darkTheme}
-      />
-      <input
-        type="button"
-        value="Danger Theme"
-        className="btn btn-danger btn-sm"
-        onClick={dangerTheme}
-      />
-      
-      {list.map(() => (
-        <div className={cardCss}>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur,
-          earum.  {cardCss}
-           <hr/> 
-          
-        </div>
-      ))}
-    </div>
-  );
+export default function App(){
+  return <div>
+    <div> Hello </div>
+    <div>Wolrd</div>
+    </div>;
 }
