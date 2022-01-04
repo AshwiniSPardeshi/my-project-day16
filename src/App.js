a9aa3ac5b74d9f8b60352669808edcb24bfc61e8
Ashwini <pardeshia1998@gmail.com>
2022-01-04 21:59:21 +0530

    Card using login and user defined input

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 // import { useState } from "react";
 
+import { useState } from "react";
+
 export default function App(){
   return ( <div className="containar-fluid" >
     <Heading/>
     <div className="row">
-      <CardD className="col-sm-12 col-md-4" />
-      <CardD className="col-sm-4 col-md-4" />
-      <CardD className="col-4 col-md-4" />
-      <CardD className="col-4 col-md-4" />
-      <CardD className="col-4 col-md-4" />
-      <CardD className="col-4 col-md-4" />
-      <CardD className="col-4 col-md-4" />
-      <CardD className="col-4 col-md-4" />
+      <div className="col-4 col-sm-12"> 
+        <CardD/>        
+      </div>
     </div>
   </div>
   );
@@ -22,11 +19,42 @@ function Heading(){
     <h1>Card Samples</h1>
   </div>
 }
+
 function CardD(){
-  return <div className="card">
-        <div className="card-header" > Title </div>
+  const list = [1,1,1,1,1,1,1];
+  const [title, setTitle] = useState("");
+
+  const Click = () => {
+    return;
+  };
+  
+  const typeTitle = (e) => {
+    setTitle(e.target.value);
+  }; 
+
+  return <div className="container-fluid bg-info">
+    <div>    
+      <input className="m-2 " type="text" value={title} placeholder="Enter Title..."  onChange={typeTitle} />
+    </div>
+    <div>
+      <input className="m-2 " type="button" value="Click" onClick={Click} />
+    </div>
+    <div>{title}</div>
+    
+    {list.map( (item) =>     <div className="card">
+        <div className="card-header" > {title} </div>
+        <div className="card-body"> <img src="https://picsum.photos/200"/> </div>
+        <div className="card-footer"> <input type="button"   value="Buy Now"/> </div>
+    </div> ) }
+
+  </div>
+
+    /*
+    <div className="card">
+        <div className="card-header" > {Title} </div>
         <div className="card-body"> <img src="https://picsum.photos/200"/> </div>
         <div className="card-footer"> <input type="button"   value="Buy Now"/> </div>
     </div>;
+    */
 
 }
