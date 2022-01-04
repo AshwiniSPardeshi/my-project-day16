6c32f3caf0fae7eca4a05b3f99aed51d5c398d56
Ashwini <pardeshia1998@gmail.com>
2022-01-04 17:29:40 +0530

    card

--- src/App.js
+++ src/App.js
@@ -1,6 +1,32 @@
+// import { useState } from "react";
+
 export default function App(){
+  return ( <div className="containar-fluid" >
+    <Heading/>
+    <div className="row">
+      <CardD className="col-sm-12 col-md-4" />
+      <CardD className="col-sm-4 col-md-4" />
+      <CardD className="col-4 col-md-4" />
+      <CardD className="col-4 col-md-4" />
+      <CardD className="col-4 col-md-4" />
+      <CardD className="col-4 col-md-4" />
+      <CardD className="col-4 col-md-4" />
+      <CardD className="col-4 col-md-4" />
+    </div>
+  </div>
+  );
+}
+
+function Heading(){
   return <div>
-    <div> Hello </div>
-    <div>Wolrd</div>
+    <h1>Card Samples</h1>
+  </div>
+}
+function CardD(){
+  return <div className="card">
+        <div className="card-header" > Title </div>
+        <div className="card-body"> <img src="https://picsum.photos/200"/> </div>
+        <div className="card-footer"> <input type="button"   value="Buy Now"/> </div>
     </div>;
+
 }
