091988f639dcf575a9badbd040ea6a58253a6146
Ashwini <pardeshia1998@gmail.com>
2022-01-04 16:19:14 +0530

    Interpolation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 export default function App(){
     return <div>
-        <div>Hello</div>
+      <MyComp/>
     </div>;
+}
+
+function MyComp(){
+  let userName = "Ashwini";
+  let id = 29;
+  let subjects = ["Core-Jave ", " Operating System ", " Data Structure ", " Data Base ", " WPT"," Advance Java"];
+
+  return <div>
+    <div>My name is {userName},</div>
+    <div>roll number is {id}</div>
+    <div>A CDAC course have a this subjects {subjects}</div>
+    <div>{subjects.map((sub) => <div> {sub} </div> )}
+    </div>
+  </div>
 }
